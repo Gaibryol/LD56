11780085dd6e05a98b999a84f6c3324e70e14a9a
akchen1 <43446512+akchen1@users.noreply.github.com>
2024-10-06 21:30:41 -0600

    adjust numbers

--- Assets/Scripts/Constants/Constants+Enemy.cs
+++ Assets/Scripts/Constants/Constants+Enemy.cs
@@ -23,7 +23,7 @@ public partial class Constants
 
     public static class EnemyManager
     {
-        public static Vector2 screenSpawnBuffer = new Vector2(0.5f, 0.5f); // Offset from the edge of the screen to start spawning
+        public static Vector2 screenSpawnBuffer = new Vector2(0f, 0f); // Offset from the edge of the screen to start spawning
         public static Vector2 maxSpawnDistanceFromBuffer = new Vector2(1, 1);
         public static Vector2 maxValidDistanceAwayFromScreen = new Vector2(4, 4);
     }
@@ -31,6 +31,6 @@ public partial class Constants
     public static class EnemyAttack
     {
         public static Vector2 maxValidDistanceAwayFromScreen = new Vector2(4, 4); // Distance from screen that an object an live
-        public static Vector2 maxValidAttackDistanceAwayFromScreen = new Vector2(0.2f, 0.2f); // Distance from from screen that an enemy can attack
+        public static Vector2 maxValidAttackDistanceAwayFromScreen = new Vector2(0f, 0f); // Distance from from screen that an enemy can attack
     }
 }
